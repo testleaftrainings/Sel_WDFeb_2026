d4d0e3310839413125b392452a6452b6081ebc19
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-02-05 14:58:32 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// we need print statement to display or confirm the execution
+		System.out.println("HI TEAM, WELCOME TO TESTLEAF");
+
+	}
+}
+
---
+++ Week1/Day1/LearnDataTypes.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class LearnDataTypes {
+	
+	public static void main(String[] args) {
+		
+		//DataTypes - primitive/Non-Primitive
+		//Primitive DataTypes - Integer, Float, Character, Boolean
+		//Non_primitive DataTypes - String, Array
+		
+		//DataType VariableName = value;
+		String userName = "Divya";
+		char lastName = 'V';
+		int age = 20;
+		boolean major = true;
+		float height = 175.8f;
+		long contactNumber = 98765432112352525l;
+		
+		System.out.println("userName "+userName);
+		System.out.println("lastName "+lastName);
+		System.out.println("age "+age);
+		System.out.println("major "+major);
+		System.out.println("height "+height);
+		System.out.println("contactNumber "+contactNumber);
+		
+			
+		
+		
+	}
+
+}
